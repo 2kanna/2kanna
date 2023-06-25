eea77bf2215db9152b87097b9330b747e84784d9
2kanna <2kanna@pm.me>
2023-06-25 23:31:20 +0100

    revert const.js API_URL

--- frontend/const.js
+++ frontend/const.js
@@ -1,4 +1,2 @@
-const API_URL = 'http://localhost:8000';
-
-// const API_URL = window.location.protocol + '//2kanna.org:'
-//     + (window.location.protocol === 'http:' ? '8080' : '4443');
+const API_URL = window.location.protocol + '//2kanna.org:'
+    + (window.location.protocol === 'http:' ? '8080' : '4443');
